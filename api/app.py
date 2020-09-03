e357ca50e3e70f987c644a6925c6424bce92e3b8
Junior Moura <junior.moura19@hotmail.com>
2020-09-03 20:34:22 -0300

    alteraçao banco

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres://root:itnisanjr19@localhost/"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres://root:senhasenha@localhost/"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
